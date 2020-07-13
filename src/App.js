41cffcb15cb067f2c066b0d9598f3abff96811c8
Javier Carreño <carrenojavier015@gmail.com>
2020-07-13 15:14:03 -0500

    Add all my files

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import './App.css';
+import { fetchBeer } from './api/fetchbeer';
+import React, { useState } from 'react';
+
+
+const App = () => {
+  const [query, setQuery] = useState('');
+  const [Beer, setBeer] = useState({});
+  
+  const search = async (e) => {
+      if(e.key === 'Enter') {
+          const data = await fetchBeer(query);
+
+          setBeer(data);
+          setQuery('');
+      }
+ 
+ 
+    }
+
+    return(
+      <div className="main-container">
+      <input type="text"className="search" placeholder="Search..."value={query}onChange={(e) => setQuery(e.target.value)}onKeyPress={search}/>
+      {Beer.main && (
+          <div className="beer">
+              <h2 className="beer-name">
+                  <span>{Beer.name}</span>
+                  <sup>{Beer.sys.type}</sup>
+              </h2>
+              <div className="beer-temp">
+                  {Math.round(Beer.main.type)}
+                  <sup>&deg;C</sup>
+              </div>
+             
+          </div>
+
+      )}
+  </div>
+
+    );
+}
+
+export default App;
