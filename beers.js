6ce9676069509bd7ac4b30053f1cde00a2d3de12
Javier  Carreño <35305955+car251290@users.noreply.github.com>
2021-09-28 18:37:00 -0500

    Add files via upload

--- beers.js
+++ beers.js
@@ -1,31 +1,38 @@
-import React, { Component } from 'react';
-
+import React from 'react';
 import '../styles/style.css'
-import '../App.css'
-
-class Beers extends Component {
 
 
-  render() {
-    const items = this.props.beers.map((beer) => {
-      return (
-        
-          <li key={beer.id}>
-          <h3>{beer.name}</h3>
-          <h4>{beer.images}</h4>
-          <p>{beer.description}</p>
-        </li>
+
+const Beers =({beers}) => {
+  return(
       
-        
-      )
-    });
-
-    return (
-      <ul>
-        {items}
-      </ul>
-    );
-  }
+      <li key={beers.id}> 
+      <h3>{beers.name}</h3>
+      <h5> {beers.website_url}</h5>
+    </li>
+    
+  )
 }
+//class Beers extends Component {
+
+ // render() {
+    //const items = this.props.beers.map((beer) => {
+      //return (
+      //  <li key={beer.id}>
+      //    <h3>{beer.name}</h3>
+      //    <h5> {beer.website_url}</h5>
+      //    <h4>{beer.images}</h4>
+      //    <p>{beer.description}</p>
+      //  </li>
+     // )
+    //});
+
+   // return (
+    //  <ul>
+    //    {items}
+    //  </ul>
+    //);
+ // }
+//}
 
 export default Beers;
