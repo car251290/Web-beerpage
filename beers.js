76eec0b771a9d421434989e9e4cd05de5ce73e5c
Javier  Carreño <35305955+car251290@users.noreply.github.com>
2021-10-12 21:05:55 -0500

    Update beers.js

--- beers.js
+++ beers.js
@@ -13,26 +13,5 @@ const Beers =({beers}) => {
     
   )
 }
-//class Beers extends Component {
-
- // render() {
-    //const items = this.props.beers.map((beer) => {
-      //return (
-      //  <li key={beer.id}>
-      //    <h3>{beer.name}</h3>
-      //    <h5> {beer.website_url}</h5>
-      //    <h4>{beer.images}</h4>
-      //    <p>{beer.description}</p>
-      //  </li>
-     // )
-    //});
-
-   // return (
-    //  <ul>
-    //    {items}
-    //  </ul>
-    //);
- // }
-//}
 
 export default Beers;
