eac5460498a9c0b9097ed2621ab60c1974113086
Javier  Carreño <35305955+car251290@users.noreply.github.com>
2021-10-13 20:05:56 -0500

    Update header.js

--- header.js
+++ header.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import {AppBar,Toolbar,Typography,InputBase,Box} from '@material-ui/core';
-import Beers from '../Beer/beers';
 import useStyles from './styles';
 const Header = () =>{
   const classes = useStyles();
